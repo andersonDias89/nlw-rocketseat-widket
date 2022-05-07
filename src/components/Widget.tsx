451f39a14af5142a5ddef81b96bd7e8ede1151f4
Anderson Dias <andersondiasmd25@gmail.com>
2022-05-07 16:57:20 -0300

    finalizando o widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,7 +1,20 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import {Popover} from '@headlessui/react' 
+
 export function Widget() {
   return (
-    <div>
-      <h1 className="text-5xl">Projeto em construção</h1>
-    </div>
+    <Popover className='absolute bottom-4 right-4'>
+      <Popover.Panel>Aberto</Popover.Panel>
+
+      <Popover.Button  className="bg-violet-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className='w-6 h-6' />
+
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+          <span className='pl-2'></span>
+
+          Feedbek
+        </span>
+      </Popover.Button>
+    </Popover>
   )
 }
